ee4380f6fb2ab9420b4f02299f50c9b00bb81e94
wbsvnetdev <wbsvnet@gmail.com>
2019-11-11 14:55:34 +0000

    Setup

---
+++ main.tf
@@ -0,0 +1,192 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+#------- IAM ---------
+
+#S3_access
+
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name  = "s3_access"
+  role  = "${aws_iam_role.s3_access_role.name}"
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name  = "s3_access_policy"
+  role  = "${aws_iam_role.s3_access_role.id}"
+
+ policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource" : "*"
+    }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "ec2.amazonaws.com"
+    },
+      "Effect": "Allow",
+      "Sid": ""
+      }
+  ]
+}
+EOF
+}
+
+
+
+#---------- VPC ----------
+
+resource "aws_vpc" "wp_vpc" {
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name = "wp_vpc"
+  }
+}
+
+
+#Internet Gateway
+
+resource "aws_internet_gateway" "wp_internet_gateway" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name = "wp_igw"
+ }
+}
+
+
+# Route tables
+
+resource "aws_route_table" "wp_public_rt" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+  }
+
+ tags {
+   Name = "wp_public"
+  }
+}
+
+resource "aws_default_route_table" "wp_private_rt" {
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+   Name = "wp_private"
+  }
+}
+
+
+#Subnets
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+   Name = "wp_public1"
+ }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+   Name = "wp_public2"
+ }
+}
+
+#Subnet wp_private1
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id ="${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["private1"]}"
+  
+map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+   Name = "wp_private1"
+ }
+}
+
+#Subnet wp_private2
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id ="${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["private2"]}"
+
+map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+   Name = "wp_private2"
+ }
+}
+
+#Subnet wp_rds1
+resource "aws_subnet" "wp_rds1_subnet" {
+  vpc_id ="${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["rds1"]}"
+
+map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+   Name = "wp_rds1"
+ }
+}
+
+
+#Subnet wp_rds2
+resource "aws_subnet" "wp_rds2_subnet" {
+  vpc_id ="${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["rds2"]}"
+
+map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+   Name = "wp_rds2"
+ }
+}
+
+#Subnet wp_rds3
+resource "aws_subnet" "wp_rds3_subnet" {
+  vpc_id ="${aws_vpc.wp_vpc.id}"
+  cidr_block = "${var.cidrs["rds3"]}"
+
+map_public_ip_on_launch = false
+  availability_zone = "${data.aws_availability_zones.available.names[2]}"
+
+  tags {
+   Name = "wp_rds3"
+ }
+}
+
