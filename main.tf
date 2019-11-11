5ee451fd3779bb43ac3979cdf5bd0c2800add2ea
wbsvnetdev <wbsvnet@gmail.com>
2019-11-11 16:26:25 +0000

    Update SG

--- main.tf
+++ main.tf
@@ -190,3 +190,114 @@ map_public_ip_on_launch = false
  }
 }
 
+
+# Security groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+
+  ingress {
+    from_port = 22
+    to_port  = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+
+  #HTTP
+
+  ingress {
+    from_port = 80
+    to_port  = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+}
+
+
+
+# Public Security Group
+
+resource "aws_security_group" "wp_public_sg"{
+  name = "wp_public_sg"
+  description = "Used for the elastic load balancer for public access"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+
+  ingress {
+    from_port = 80
+    to_port  = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+}
+
+
+#Private Security Group
+
+resource "aws_security_group" "wp_private_sg" {
+  name = "wp_private_sg"
+  description = "Used for the private instances"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #ALL TRAFFIC
+  
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}  
+
+#RDS Security Group
+
+resource "aws_security_group" "wp_rds_sg" {
+  name = "wp_rds_sg"
+  description = "Used for RDS instances"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # SQL access from public/private security groups
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}"
+    ]
+  }
+ }
+
+ 
